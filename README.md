c42b201038df113835b787a62bc5978b7b96517c
TimaR0 <trodoslavov@gmail.com>
2023-03-27 23:19:06 +0300

    dz 9.03.2023

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # SystemProgramming-111
 
 
-1. клик на старт и потом стоп
+1. клик на старт и потом стоп (скриншот во время разработке)
 
-![name](./Screenshot/Screenshot_7.png)
+![name](./Screenshot/Screenshot_8.png)
+
+2. клик на старт и потом стоп (финаверсия версия в комите)
+
+![name](./Screenshot/Screenshot_9.png)
 
--- TaskWindow.xaml.cs
+++ TaskWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace SystemProgramming_111
     /// </summary>
     public partial class TaskWindow : Window
     {
+        Random random = new Random();
         private void ConsoleWrite(object stringLike)
         {
 
@@ -155,7 +156,7 @@ namespace SystemProgramming_111
             {
                     ConsoleWrite(await task);
             }
-//            Task.WhenAll
+
         }
         private void ProcN(object? item)
         {
@@ -174,6 +175,86 @@ namespace SystemProgramming_111
 
             return $"Func{N} return\n";
         }
+
+        #region dz 9.03.2023
+
+        CancellationTokenSource cts5;
+        private async void ButtonStart5_Click(object sender, RoutedEventArgs e)
+        {
+            cts5 = new CancellationTokenSource();
+            int i = 0;
+            int allMounthCount = 25;
+            ConsoleBlock.Text = "";
+            progressBar5.Value = 0;
+            sum5 = 100;
+            try
+            {
+                for (; i < allMounthCount; i++)
+                {
+                    await plusPercent5(new TaskData5
+                    {
+                        Month = i,
+                        Token = cts5.Token
+                    });
+                    cts5.Token.ThrowIfCancellationRequested();
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    ConsoleBlock.Text += i + "/" + allMounthCount + " Canceled" + "\n";
+                    progressBar5.Value += 100.0 / 25;
+                });
+            }
+        }
+        private void ButtonStop5_Click(object sender, RoutedEventArgs e)
+        {
+            cts5?.Cancel();
+        }
+        private double sum5;
+        private readonly object locker5 = new();     // объект для синхронизации
+        private async Task plusPercent5(object? data)
+        {
+            var threadData = data as TaskData5;
+            if (threadData is null) return;
+            try
+            {
+
+                double val;
+
+                await Task.Delay(random.Next(250, 350));
+                // место для возможной отмены потока
+                threadData.Token.ThrowIfCancellationRequested();
+                double percent = 10;
+                double factor = 1 + percent / 100;
+                lock (locker5)
+                {                                      // внутри блока
+                    val = sum5;                        // остается часть рассчетов
+                    val *= factor;                     // которую нельзя более
+                    sum5 = val;                        // разделять
+                }
+                Dispatcher.Invoke(() =>
+                {
+                    ConsoleBlock.Text += threadData.Month + " " + percent + " " + val + "\n";
+                    progressBar5.Value += 100.0 / 25;
+                });
+            }
+            catch (OperationCanceledException)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    ConsoleBlock.Text += threadData.Month + " Canceled" + "\n";
+                    progressBar5.Value += 100.0 / 25;
+                });
+            }
+        }
+        class TaskData5   // комплексный тип для передачи нескольких данных в задачю
+        {
+            public int Month { get; set; }
+            public CancellationToken Token { get; set; }
+        }
+        #endregion dz 9.03.2023
     }
-   
+
 }
